f9cd9e298e0b543f72cd3f6034c534845618c52b
adastrafll <adastrafll@gmail.com>
2024-02-11 16:22:52 -0500

    Changes in profiles and redirecting in the home logo to home instead of pounded bookmark.

--- login.js
+++ login.js
@@ -10,8 +10,25 @@ const onSubmit = (e) => {
 }
 
 const login = (email, password) => {
+  email=email.toLowerCase();
   switch(email) {
     case 'anishhaha3@gmail.com': 
+      localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
+      window.location.href = '/anish/home.html';
+      break;
+      case 'rishik.shukla86@masonohioschools.com': 
+      localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
+      window.location.href = '/rishik/home.html';
+      break;
+      case 'prishaupadhyay6861@gmail.com': 
+      localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
+      window.location.href = '/prisha/home.html';
+      break;
+      case 'vibhav.vadakapurapu75@masonohioschools.com': 
+      localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
+      window.location.href = '/anish/home.html';
+      break;
+      case 'akshajkulkarni2022@gmail.com': 
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
       window.location.href = '/anish/home.html';
       break;
