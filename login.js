a384fe9703636e75f3128fa245d382b0fd427bc0
adastrafll <adastrafll@gmail.com>
2024-02-17 15:45:06 -0500

    Made the login better yay

--- login.js
+++ login.js
@@ -14,15 +14,15 @@ const login = (email, password) => {
   switch(email){
     case 'anishhaha3@gmail.com': 
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
-      window.location.href = '/anish/home.html';
+      window.location.href = './anish/home.html';
       break;
       case 'rishik.shukla86@masonohioschools.com': 
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
-      window.location.href = '/rishik/home.html';
+      window.location.href = './rishik/home.html';
       break;
       case 'prishaupadhyay6861@gmail.com': 
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
-      window.location.href = '/prisha/home.html';
+      window.location.href = './prisha/home.html';
       break;
       case 'vibhav.vadakapurapu75@masonohioschools.com': 
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
@@ -36,4 +36,7 @@ const login = (email, password) => {
       alert('Invalid email or password');
   }
   return false;
+}
+const onLogin = () => {
+  window.location.href = './login.html';
 }
