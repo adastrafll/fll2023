f93a6812f4c469b4152a5f448587a06d9da687ed
adastrafll <adastrafll@gmail.com>
2024-03-02 17:49:26 -0500

    lots and lots of changes

--- login.js
+++ login.js
@@ -36,7 +36,7 @@ const login = (email, password) => {
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
       window.location.href = '/anish/home.html';
       break;
-      case 'guest@guest.com':
+      case 'guest':
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
       window.location.href = '/rishik/profile.html';
       break;
