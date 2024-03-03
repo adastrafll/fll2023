31448e1a85d383b83386c85a82297aef795bdd01
adastrafll <adastrafll@gmail.com>
2024-03-03 13:28:33 -0500

    32kn;lf

--- login.js
+++ login.js
@@ -30,7 +30,7 @@ const login = (email, password) => {
       break;
       case 'vibhav.vadakapurapu75@masonohioschools.com': 
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
-      window.location.href = '/vibhav/vibhavprofile.html';
+      window.location.href = '/vibhav/  profile.html';
       break;
       case 'akshajkulkarni2022@gmail.com': 
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
@@ -38,7 +38,7 @@ const login = (email, password) => {
       break;
       case 'guest':
       localStorage.setItem('user', JSON.stringify({ email, name: 'Anish Mahajan' }));
-      window.location.href = '/rishik/profile.html';
+      window.location.href = '/guest/profile.html';
       break;
     default:
       alert('Invalid email or password');
