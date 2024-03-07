a686821f04c8971393e14a60722263fe05f2d498
mahajanvm <vikasvm@gmail.com>
2024-03-06 23:02:27 -0500

    Update header.js

--- header.js
+++ header.js
@@ -44,5 +44,5 @@ const setTheme = (theme) => {
 };
 const onLogout = () => {
     localStorage.removeItem('user');
-    window.location.href = './index.html';
+    window.location.href = '../index.html';
 }
